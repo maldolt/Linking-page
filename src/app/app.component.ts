686ca78ea7bad1978feace8af4149a893dc24bd2
Laura Velasco <maldolt@nv.ccsd.net>
2024-01-09 19:25:51 -0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,9 @@ export class AppComponent {
   challenge: string = 'Cup Flip'
   username: string = 'Brando'
   videoSource: string = 'http://54.89.2.39/storage/challenge_videos/brando_1.mp4'
-
+  // You can add a method to handle the download action
+  downloadApp() {
+    // Add your download logic here
+    console.log('Downloading the app...');
+  }
 }
