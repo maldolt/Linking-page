53a9b4b32edb3dcf63d0da3a2548f72e97977dfd
Laura Velasco <lauratvelasco@gmail.com>
2024-01-30 17:57:37 -0800

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, VideoplayerComponent],
+  declarations: [AppComponent, VideoplayerComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
